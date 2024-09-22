cb3f2f13a45a61e23d55c1b8b2d7c1f600e99c21
Marina Hristova <mhristova@lelypartners.com>
2024-09-22 17:22:51 +0300

    Add services

--- src/services/s3Service.ts
+++ src/services/s3Service.ts
@@ -8,15 +8,59 @@ const s3 = new AWS.S3({
 
 const bucketName = process.env.REACT_APP_AWS_BUCKET_NAME;
 
-export const getFiles = async () => {
+export const getFiles = async (prefix = "") => {
+  const params = {
+    Bucket: bucketName as string,
+    Prefix: prefix,
+    Delimiter: "/",
+  };
   try {
-    const params = {
-      Bucket: bucketName as string,
-    };
     const response = await s3.listObjectsV2(params).promise();
-    return response.Contents;
+
+    const folders =
+      response.CommonPrefixes?.map((folder) => ({
+        name: folder.Prefix,
+        type: "folder",
+      })) || [];
+
+    const files =
+      response.Contents?.filter((file) => file.Key !== prefix).map((file) => ({
+        name: file.Key,
+        type: "file",
+      })) || [];
+
+    return [...folders, ...files];
   } catch (error) {
     alert("error");
     throw error;
+  }
+};
+
+export const addFile = async (file: File) => {
+  const params = {
+    Bucket: bucketName as string,
+    Key: file.name,
+    Body: file,
+  };
+
+  try {
+    await s3.upload(params).promise();
+    //TODO Successfully added file
+  } catch (error) {
+    //TODO: Error handling
+  }
+};
+
+export const deleteFile = async (filePath: string) => {
+  const params = {
+    Bucket: bucketName as string,
+    Key: filePath,
+  };
+
+  try {
+    await s3.deleteObject(params).promise();
+    //TODO Successfully deleted file
+  } catch (error) {
+    //TODO: Error handling
   }
 };
